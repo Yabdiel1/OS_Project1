3baceb28c40d28b3637729b3023266019e233abe
yamilmorales <yamilgabriel5663@gmail.com>
2024-11-23 04:46:07 -0400

    Fix conversion from absolute to terminal position

--- mouse_saver/src/main.c
+++ mouse_saver/src/main.c
@@ -7,8 +7,8 @@
 
 int abs_x;
 int abs_y;
-int terminal_x;
-int terminal_y;
+float terminal_x;
+float terminal_y;
 FILE *file = NULL;
 
 void HandleSigint(int sig) {
@@ -51,15 +51,25 @@ int main() {
       abs_x += x_movement;
       int y_movement = (int8_t)data[2];
       abs_y += y_movement;
-      terminal_x = abs(abs_x) % 100;
-      terminal_y = abs(abs_y) % 25;
+
+      if (abs_x <= 0) {
+        terminal_x = (512 - abs(abs_x)) / 10.24;
+      } else {
+        terminal_x = (512 + abs_x) / 10.24;
+      }
+
+      if (abs_y < 0) {
+        terminal_y = (384 + abs(abs_y)) / 30.72;
+      } else {
+        terminal_y = (384 - abs_y) / 30.72;
+      }
 
       // Save coordinates to the binary file
-      fwrite(&terminal_x, sizeof(int), 1, file);
-      fwrite(&terminal_y, sizeof(int), 1, file);
+      fwrite(&terminal_x, sizeof(float), 1, file);
+      fwrite(&terminal_y, sizeof(float), 1, file);
 
       printf(
-          "X: %d, Y: %d (Absolute Position) | X: %d, Y: %d (Terminal "
+          "X: %d, Y: %d (Absolute Position) | X: %f, Y: %f (Terminal "
           "Position)\n",
           abs_x, abs_y, terminal_x, terminal_y);
 
